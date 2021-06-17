dd9bfa304af5f95713927752210009dbb2ee9c8a
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-17 23:03:53 +0530

    [Anjaneya]Added:User email validation

--- RegexProgram/RegexProgram/Pattern.cs
+++ RegexProgram/RegexProgram/Pattern.cs
@@ -9,6 +9,7 @@ namespace RegexProgram
     {
         public string Regex_FirstName = "^[A-Z][a-z]{3,}$";
         public string Regex_LastName = "^[A-Z][a-z]{3,}$";
+        public string Regex_EmailId = "^[a-zA-Z0-9]+([.][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]+)?$";
         public bool Validate_firstName(string firstName)
         {
             return Regex.IsMatch(firstName, Regex_FirstName);
@@ -17,6 +18,10 @@ namespace RegexProgram
         {
             return Regex.IsMatch(lastName, Regex_LastName);
         } 
+        public bool Validate_Email(string EmailId)
+        {
+            return Regex.IsMatch(EmailId, Regex_EmailId);
+        }
 
     }
 }
--- RegexProgram/RegexProgram/Program.cs
+++ RegexProgram/RegexProgram/Program.cs
@@ -10,6 +10,8 @@ namespace RegexProgram
             Pattern pattern = new Pattern();
             Console.WriteLine(pattern.Validate_firstName("Anjaneya"));
             Console.WriteLine(pattern.Validate_LastName("Tosh"));
+            Console.WriteLine(pattern.Validate_Email("abc.xyz@bl.co.in"));
+
         }
     }
 }
